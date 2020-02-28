6b318db47c99f725a58bf026ba971758f3178731
unknown <zulfahidayat13@gmail.com>
2020-02-28 16:25:12 +0700

    28/02/2020

--- app/src/main/java/com/zulfa/furnitureapp/DetailActivity.java
+++ app/src/main/java/com/zulfa/furnitureapp/DetailActivity.java
@@ -18,24 +18,59 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.daimajia.slider.library.Animations.DescriptionAnimation;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.HashMap;
+
 public class DetailActivity extends AppCompatActivity {
 
-    Button pesan;
-    public static final String PREFS_NAME = "LoginPrefs";
-    MainActivity mainActivity;
-    TextView nama;
+    private SliderLayout sliderLayout1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        nama = findViewById(R.id.tv_nama);
-        pesan = findViewById(R.id.btn_pesan);
+        sliderLayout1 = (SliderLayout) findViewById(R.id.slideDetail);
+        imageslider();
+    }
+
+    private void imageslider() {
+        // Load image dari URL
+        HashMap<String,String> url_maps = new HashMap<String, String>();
+        url_maps.put("Hannibal", "http://static2.hypable.com/wp-content/uploads/2013/12/hannibal-season-2-release-date.jpg");
+        url_maps.put("Big Bang Theory", "http://tvfiles.alphacoders.com/100/hdclearart-10.png");
+        url_maps.put("House of Cards", "http://cdn3.nflximg.net/images/3093/2043093.jpg");
+        url_maps.put("Game of Thrones", "http://images.boomsbeat.com/data/images/full/19640/game-of-thrones-season-4-jpg.jpg");
+        // Load Image Dari res/drawable
+        HashMap<String,Integer> file_maps = new HashMap<String, Integer>();
+        file_maps.put("Modern Chair",R.drawable.modernchair);
+        file_maps.put("Modern",R.drawable.modernchair2);
+        file_maps.put("Chair",R.drawable.modernchair3);
+//        file_maps.put("Mendem Anggur Lur?", R.drawable.anggur);
+        for(String name : file_maps.keySet()){
+            TextSliderView textSliderView = new TextSliderView(this);
+            // initialize a SliderLayout
+            textSliderView
+                    .description(name)
+                    .image(file_maps.get(name))
+                    .setScaleType(BaseSliderView.ScaleType.Fit);
+            //add your extra information
+            textSliderView.bundle(new Bundle());
+            textSliderView.getBundle()
+                    .putString("extra",name);
+            sliderLayout1.addSlider(textSliderView);
+        }
+        sliderLayout1.setPresetTransformer(SliderLayout.Transformer.Accordion);
+        sliderLayout1.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
+        sliderLayout1.setCustomAnimation(new DescriptionAnimation());
+        sliderLayout1.setDuration(4000);
     }
 }
--- app/src/main/java/com/zulfa/furnitureapp/HomeFragment.java
+++ app/src/main/java/com/zulfa/furnitureapp/HomeFragment.java
@@ -8,18 +8,29 @@ import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 import android.widget.ViewFlipper;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+
+
+import com.daimajia.slider.library.Animations.DescriptionAnimation;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
-    private ViewFlipper ViewF;
-    private Animation fadeIn;
-    private Animation fadeOut;
-    ImageView gambar;
+    private CardView cardView;
+    private SliderLayout sliderLayout;
 
     @Nullable
     @Override
@@ -30,22 +41,56 @@ public class HomeFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        ViewF = (ViewFlipper) view.findViewById(R.id.ViewFlipper);
-        fadeIn = AnimationUtils.loadAnimation(getContext(),R.anim.fade_in);
-        fadeOut = AnimationUtils.loadAnimation(getContext(),R.anim.fade_out);
-        gambar = view.findViewById(R.id.gmbr);
-        gambar.setOnClickListener(new View.OnClickListener() {
+        sliderLayout = (SliderLayout) view.findViewById(R.id.slider);
+        cardView = view.findViewById(R.id.card1);
+        imageslider();
+        onClick();
+    }
+
+    private void onClick() {
+        cardView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 startActivity(new Intent(getContext(), DetailActivity.class));
             }
         });
+    }
 
-        ViewF.setInAnimation((Animation) fadeIn);
-        ViewF.setOutAnimation((Animation) fadeOut);
-
-        ViewF.setAutoStart(true);
-        ViewF.setFlipInterval(5000);
-        ViewF.startFlipping();
+    private void imageslider() {
+        // Load image dari URL
+        HashMap<String,String> url_maps = new HashMap<String, String>();
+        url_maps.put("Hannibal", "http://static2.hypable.com/wp-content/uploads/2013/12/hannibal-season-2-release-date.jpg");
+        url_maps.put("Big Bang Theory", "http://tvfiles.alphacoders.com/100/hdclearart-10.png");
+        url_maps.put("House of Cards", "http://cdn3.nflximg.net/images/3093/2043093.jpg");
+        url_maps.put("Game of Thrones", "http://images.boomsbeat.com/data/images/full/19640/game-of-thrones-season-4-jpg.jpg");
+        // Load Image Dari res/drawable
+        HashMap<String,Integer> file_maps = new HashMap<String, Integer>();
+        file_maps.put("Diskon Besar Besaran",R.drawable.pertama);
+        file_maps.put("Ayo Makan",R.drawable.kedua);
+        file_maps.put("Ayo njoget",R.drawable.ketiga);
+        file_maps.put("Mendem Anggur Lur?", R.drawable.anggur);
+        for(String name : file_maps.keySet()){
+            TextSliderView textSliderView = new TextSliderView(getContext());
+            // initialize a SliderLayout
+            textSliderView
+                    .description(name)
+                    .image(file_maps.get(name))
+                    .setOnSliderClickListener(new BaseSliderView.OnSliderClickListener() {
+                        @Override
+                        public void onSliderClick(BaseSliderView slider) {
+                            Toast.makeText(getContext(), "klik", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .setScaleType(BaseSliderView.ScaleType.Fit);
+            //add your extra information
+            textSliderView.bundle(new Bundle());
+            textSliderView.getBundle()
+                    .putString("extra",name);
+            sliderLayout.addSlider(textSliderView);
+        }
+        sliderLayout.setPresetTransformer(SliderLayout.Transformer.Accordion);
+        sliderLayout.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
+        sliderLayout.setCustomAnimation(new DescriptionAnimation());
+        sliderLayout.setDuration(4000);
     }
 }
